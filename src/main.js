cc3287a403529ce2b6f7bfa5bac25fa8ba46328c
Jihed-Khelifi <jihedkhlifi0000@gmail.com>
2022-07-26 05:09:18 +0100

    Removed unnecessary pkg

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import Header from "./components/layout/AppHeader.vue"
@@ -7,9 +6,7 @@ import Footer from "./components/layout/AppFooter.vue"
 import About from "./components/about/AppAbout.vue"
 import AppHome from "./views/AppHome.vue"
 const app = createApp(App)
-const pinia = createPinia();
 app.use(router)
-app.use(pinia)
 app.component('AppHeader', Header)
 app.component('AppAbout', About)
 app.component('AppHome', AppHome)
